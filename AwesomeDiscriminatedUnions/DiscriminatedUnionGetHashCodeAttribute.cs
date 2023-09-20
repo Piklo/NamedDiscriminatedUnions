0d5d85f775f6c1a36883d42f72a029b2eeb1fcd5
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-20 16:00:28 +0200

    added DiscriminatedUnionGetHashCodeAttribute added DiscriminatedUnionGetHashCodeType

---
+++ AwesomeDiscriminatedUnions/DiscriminatedUnionGetHashCodeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AwesomeDiscriminatedUnions;
+
+[AttributeUsage(AttributeTargets.Struct)]
+public sealed class DiscriminatedUnionGetHashCodeAttribute : Attribute
+{
+    private readonly DiscriminatedUnionGetHashCodeType _type;
+
+    public DiscriminatedUnionGetHashCodeAttribute(DiscriminatedUnionGetHashCodeType type)
+    {
+        _type = type;
+    }
+}
+
+
+public enum DiscriminatedUnionGetHashCodeType
+{
+    /// <summary>
+    /// Do not generate GetHashCode method
+    /// </summary>
+    None,
+    /// <summary>
+    /// Generate GetHashCode method based on the item value and the tag value
+    /// </summary>
+    Strict,
+    /// <summary>
+    /// Generate GetHashCode method based on only the item value
+    /// </summary>
+    Weak,
+}
