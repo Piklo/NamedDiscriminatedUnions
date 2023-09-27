32039ecb2e5249f33d43b559820389204543abe4
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-27 16:28:13 +0200

    exposed AwesomeDiscriminatedUnions internals to AwesomeDiscriminatedUnions.Tests added AwesomeDiscriminatedUnions.Tests project

---
+++ AwesomeDiscriminatedUnions.Tests/GeneratorRunner.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace AwesomeDiscriminatedUnions.Tests;
+
+internal static class GeneratorRunner
+{
+    internal static string GetGeneratedOutput<T>(string source)
+        where T : IIncrementalGenerator, new()
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var references = new List<MetadataReference>();
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assembly in assemblies)
+        {
+            if (!assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
+            {
+                references.Add(MetadataReference.CreateFromFile(assembly.Location));
+            }
+        }
+
+        var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new T();
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+
+        Assert.False(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), $"Failed: {diagnostics.FirstOrDefault()?.GetMessage()}");
+
+        var output = outputCompilation.SyntaxTrees.Last().ToString();
+
+        return output;
+    }
+}
