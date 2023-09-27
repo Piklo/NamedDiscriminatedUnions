694b37a6cc23fafcd068bc3cce0446b3a2a8cabb
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-27 18:11:07 +0200

    updated Generator to use IndentedTextWriter

---
+++ AwesomeDiscriminatedUnions/IndentedTextWriterExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.CodeDom.Compiler;
+
+namespace AwesomeDiscriminatedUnions;
+internal static class IndentedTextWriterExtensions
+{
+    public static void WriteLineNoTabs(this IndentedTextWriter writer)
+    {
+        writer.WriteLineNoTabs(string.Empty);
+    }
+}
