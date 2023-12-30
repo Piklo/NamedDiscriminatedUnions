b51a7c1ed37752a8fe6740bde785f219696321a4
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-30 21:23:55 +0100

    added tag field test

---
+++ AwesomeDiscriminatedUnions.Generator.Tests/GeneratorTests/TagFieldTests.cs
@@ -0,0 +1,25 @@
+﻿namespace AwesomeDiscriminatedUnions.Generator.Tests.GeneratorTests;
+
+public static class TagFieldTests
+{
+    [Fact]
+    public static void FieldEnumGeneratesCorrectly()
+    {
+        const string source = """
+                    using AwesomeDiscriminatedUnions.Attributes;
+                                        
+                    namespace TestNamespace;
+
+                    [DiscriminatedUnion]
+                    public readonly partial struct Union
+                    {
+                        private readonly int _value;
+                    }
+                    """;
+        const string generatedFileName = "Union.g.cs";
+
+        var res = GeneratorRunner.GetGeneratedOutput<UnionGenerator>(source, generatedFileName)[generatedFileName];
+
+        res.Should().Contain("private readonly Tag tag;");
+    }
+}
