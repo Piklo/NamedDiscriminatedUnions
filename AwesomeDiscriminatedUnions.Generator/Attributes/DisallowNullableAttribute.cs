182eb0d61cf0b882b176df0b779e04b620316640
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-30 20:05:25 +0100

    suppressed IDE0060:Remove unused parameter for DisallowNullableAttribute constructor throwIfNull parameter

--- AwesomeDiscriminatedUnions.Generator/Attributes/DisallowNullableAttribute.cs
+++ AwesomeDiscriminatedUnions.Generator/Attributes/DisallowNullableAttribute.cs
@@ -5,6 +5,7 @@ namespace AwesomeDiscriminatedUnions.Attributes;
 [AttributeUsage(AttributeTargets.Field)]
 public sealed class DisallowNullableAttribute : Attribute
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "used by source generator")]
     public DisallowNullableAttribute(bool throwIfNull = true)
     {
     }
