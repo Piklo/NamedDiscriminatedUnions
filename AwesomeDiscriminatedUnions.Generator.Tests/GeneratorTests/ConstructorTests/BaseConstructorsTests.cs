a17bb4d1c917768efc714a8b2d754ad58d7b9474
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-31 19:26:58 +0100

    added more constructor tests

--- AwesomeDiscriminatedUnions.Generator.Tests/GeneratorTests/ConstructorTests/BaseConstructorsTests.cs
+++ AwesomeDiscriminatedUnions.Generator.Tests/GeneratorTests/ConstructorTests/BaseConstructorsTests.cs
@@ -24,6 +24,57 @@ public static class BaseConstructorsTests
     }
 
     [Fact]
+    public static void ConstructorWithMultipleParameters()
+    {
+        const string source = """
+                    using AwesomeDiscriminatedUnions.Attributes;
+                                        
+                    namespace TestNamespace;
+
+                    [DiscriminatedUnion]
+                    public readonly partial struct Union
+                    {
+                        private readonly int value;
+                        private readonly long value2;
+                    }
+                    """;
+        const string generatedFileName = "Union.g.cs";
+
+        var res = GeneratorRunner.GetGeneratedOutput<UnionGenerator>(source, generatedFileName)[generatedFileName];
+
+        res.Should().Contain("private Union(Tag tag, int value, long value2)");
+    }
+
+    [Fact]
+    public static void ConstructorAssignsValues()
+    {
+        const string source = """
+                    using AwesomeDiscriminatedUnions.Attributes;
+                                        
+                    namespace TestNamespace;
+
+                    [DiscriminatedUnion]
+                    public readonly partial struct Union
+                    {
+                        private readonly int value;
+                        private readonly long value2;
+                    }
+                    """;
+        const string generatedFileName = "Union.g.cs";
+
+        var res = GeneratorRunner.GetGeneratedOutput<UnionGenerator>(source, generatedFileName)[generatedFileName];
+
+        res.Should().Contain("""
+                private Union(Tag tag, int value, long value2)
+                {
+                    this.tag = tag;
+                    this.value = value;
+                    this.value2 = value2;
+                }
+            """);
+    }
+
+    [Fact]
     public static void NotValueTypeParameterIsNotNullable()
     {
         const string source = """
