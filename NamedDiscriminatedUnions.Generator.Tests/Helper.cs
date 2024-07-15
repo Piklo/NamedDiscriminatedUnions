aeba8c7c0dcbc04397f84a0167e27252ec3f5204
Piklo <16057987+Piklo@users.noreply.github.com>
2024-07-15 20:26:25 +0200

    made AppendHeader, AppendNamespace, AppendDeclaration, GetFullTypeNameWithGenerics internal and updated their parameters to require as little data as possible to make testing easier updated and extended BaseTests for header, namespace and declaration added Helper class to test project for easier IndentedTextWriter creation

---
+++ NamedDiscriminatedUnions.Generator.Tests/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System.CodeDom.Compiler;
+
+namespace NamedDiscriminatedUnions.Generator.Tests;
+internal static class Helper
+{
+    public static IndentedTextWriter GetIndentedTextWriter()
+    {
+        var baseWriter = new StringWriter();
+        var writer = new IndentedTextWriter(baseWriter, new string(' ', 4));
+
+        return writer;
+    }
+}
